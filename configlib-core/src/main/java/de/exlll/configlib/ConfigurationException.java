d7010ccb389d38bf38243e08119689db32140bc7
Thomas Wearmouth <tomwmth@pm.me>
2025-03-13 04:15:00 +0800

    Make ConfigurationException constructors public

--- configlib-core/src/main/java/de/exlll/configlib/ConfigurationException.java
+++ configlib-core/src/main/java/de/exlll/configlib/ConfigurationException.java
@@ -4,11 +4,11 @@ package de.exlll.configlib;
  * Signals that an error occurred during the serialization or deserialization of a configuration.
  */
 public final class ConfigurationException extends RuntimeException {
-    ConfigurationException(String message) {
+    public ConfigurationException(String message) {
         super(message);
     }
 
-    ConfigurationException(String message, Throwable cause) {
+    public ConfigurationException(String message, Throwable cause) {
         super(message, cause);
     }
 }
